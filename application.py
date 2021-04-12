206910bbc3f8c48f58a3f6bd014888a27868cce7
곽영호 <1080609@hanwha.com>
2021-04-12 12:34:59 +0900

    EB 배포

--- application.py
+++ application.py
@@ -3,16 +3,24 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import os
 from flaskext.mysql import MySQL
+import redis
 
 application = Flask(__name__)
+
+# cors
 cors = CORS(application, resources={r"/*": {"origins": "*"}})
 
+# mysql
 mysql = MySQL()
 application.config['MYSQL_DATABASE_USER'] = os.environ["MYSQL_DATABASE_USER"]
 application.config['MYSQL_DATABASE_PASSWORD'] = os.environ["MYSQL_DATABASE_PASSWORD"]
 application.config['MYSQL_DATABASE_DB'] = os.environ["MYSQL_DATABASE_DB"]
 application.config['MYSQL_DATABASE_HOST'] = os.environ["MYSQL_DATABASE_HOST"]
 mysql.init_app(application)
+
+# redis
+db = redis.Redis('sparta-redis.8imnfo.0001.apn2.cache.amazonaws.com')
+
 
 @application.route('/')
 def main():
@@ -33,12 +41,21 @@ def file_upload():
         ContentType=file.content_type
     )
 
+    # mysql
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute("insert into file(file_name) value('" + file.filename + "')")
     conn.commit()
 
+    # redis
+    cursor.execute("SELECT count(*) AS COUNT from file")
+    data = cursor.fetchone()
+    db.set("fileCount", data['COUNT'])
     return jsonify({'result': 'success'})
+
+@application.route('/file/count', methods=['GET'])
+def file_count():
+    return jsonify({'result': 'success', 'count': db.get("fileCount")})
 
 if __name__ == '__main__':
     application.debug = True
